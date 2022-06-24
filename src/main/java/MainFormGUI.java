9a4201d5202e9b310165efe97651bf24ce9c8969
Nait <floop.41@mail.ru>
2022-06-24 20:58:35 +0300

    ---

---
+++ src/main/java/MainFormGUI.java
@@ -0,0 +1,99 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.FileNotFoundException;
+import javax.swing.*;
+
+public class MainFormGUI extends JFrame{
+    private JButton button = new JButton("Вычислить");
+
+    private JTextField textField_1 = new JTextField("", 20);
+    private JLabel label_1 = new JLabel("Дебет/Счет");
+
+    private JTextField textField_2 = new JTextField("", 20);
+    private JLabel label_2 = new JLabel("Кредит/Счет");
+
+    private JTextField textField_3 = new JTextField("", 20);
+    private JLabel label_3 = new JLabel("Название компании");
+
+    private JLabel label_4 = new JLabel("Контрольная сумма");
+
+    Box mainBox, box_1, box_2, box_3, box_4;
+    public MainFormGUI () {
+        super();
+        Font font = new Font("Verdana", Font.BOLD, 16);
+        this.setIconImage(new ImageIcon("D:\\parser\\icon1.png").getImage());
+        setLocation(900, 400);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        label_4.setFont(font);
+        label_4.setAlignmentX(CENTER_ALIGNMENT);
+        label_3.setAlignmentX(CENTER_ALIGNMENT);
+        label_2.setAlignmentX(CENTER_ALIGNMENT);
+        label_1.setAlignmentX(CENTER_ALIGNMENT);
+        button.setAlignmentX(CENTER_ALIGNMENT);
+        mainBox = Box.createVerticalBox();
+        box_1 = Box.createVerticalBox();
+        box_2 = Box.createVerticalBox();
+        box_3 = Box.createVerticalBox();
+        box_4 = Box.createVerticalBox();
+
+        box_1.add(label_4);
+        box_1.add(Box.createVerticalStrut(10));
+        box_2.add(label_1);
+        box_2.add(textField_1);
+        box_3.add(label_2);
+        box_3.add(textField_2);
+        box_3.add(label_3);
+        box_3.add(textField_3);
+
+        mainBox.add(box_1);
+        mainBox.add(box_2);
+        mainBox.add(box_3);
+        mainBox.add(box_4);
+        mainBox.add(button);
+
+        button.addActionListener(new ButtonEventListener());
+
+        setContentPane(mainBox);
+        setVisible(true);
+        pack();
+    }
+
+    class ButtonEventListener implements ActionListener {
+        public void actionPerformed (ActionEvent e){
+            SumResearch sumResearch = new SumResearch();
+            try {
+                ResFormGUI resFormGUI = new ResFormGUI(sumResearch.findRes(textField_1.getText(), textField_2.getText(), textField_3.getText()));
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    public class ResFormGUI extends JFrame{
+
+        private JTextArea textField = new JTextArea();
+
+        private JLabel label = new JLabel("Результат");
+
+        private JScrollPane scroll;
+
+        Box mainBox;
+        public ResFormGUI (String text) {
+            super();
+            Font font = new Font("Verdana", Font.BOLD, 16);
+            label.setFont(font);
+            label.setAlignmentX(CENTER_ALIGNMENT);
+            this.setIconImage(new ImageIcon("D:\\parser\\icon1.png").getImage());
+            setSize(500, 250);
+            setLocation(900, 400);
+            textField.setText(text);
+            scroll = new JScrollPane(textField);
+            mainBox = Box.createVerticalBox();
+            mainBox.add(label);
+            mainBox.add(Box.createVerticalStrut(10));
+            mainBox.add(scroll);
+            setContentPane(mainBox);
+            setVisible(true);
+        }
+    }
+}
---
+++ src/main/java/SumResearch.java
@@ -0,0 +1,111 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SumResearch {
+    String findRes(String fieldFirst, String fieldSecond, String nameCompany) throws FileNotFoundException {
+        String result = "";
+        ArrayList<ArrayList<String>> answerList = new ArrayList<ArrayList<String>>();
+        File file = new File("D:\\parser\\book14.TXT");
+        Scanner scanner = new Scanner(file);
+        while(scanner.hasNextLine()){
+            String check = scanner.nextLine();
+            if(check.toCharArray()[1] >= '0' && check.toCharArray()[1] <= '9'){
+                if(check.contains(fieldFirst) && check.contains(fieldSecond)) {
+                    System.out.println(check);
+                    char[] fullLineFirst = check.toCharArray();
+                    int ccch = check.indexOf("\t" + fieldSecond + "\t");
+                    check = scanner.nextLine();
+                    check = scanner.nextLine();
+                    check = scanner.nextLine();
+                    System.out.println(check);
+                    System.out.println("\n");
+                    char[] fullLineSecond = check.toCharArray();
+                    char[] dataLine = new char[10];
+                    char[] nameLine = new char[fullLineSecond.length-4];
+                    char[] sumLine = new char[50];
+                    int counterT = 0;
+                    for(int t = 0; fullLineFirst[t] != ','; t++){
+                        counterT++;
+                    }
+                    counterT += 3;
+                    int w = 0;
+                    for(int t = counterT; t > ccch + 4; t--, w++){
+                        sumLine[w] = fullLineFirst[t];
+                        //System.out.print(sumLine[w]);
+                    }
+                    for (int t = 0; t < sumLine.length / 2; t++) {
+                        char tmp = sumLine[t];
+                        sumLine[t] = sumLine[sumLine.length - t - 1];
+                        sumLine[sumLine.length - t - 1] = tmp;
+                    }
+                    String bufSum = "";
+                    for(int t = 0; t<sumLine.length;t++){
+                        if(sumLine[t]==','){
+                            bufSum += '.';
+                        }
+                        if(sumLine[t] >= '0' && sumLine[t] <= '9'){
+                            bufSum += sumLine[t];
+                        }
+                    }
+                    for(int t = 1; t<11;t++)
+                    {
+                        dataLine[t-1] = fullLineFirst[t];
+                    }
+                    for(int t = 4; t<fullLineSecond.length-1;t++)
+                    {
+                        nameLine[t-4] = fullLineSecond[t];
+                    }
+                    String strNameLine = "";
+                    for(int t = 1; nameLine[t - 1] != '\t' ; t++){
+                        strNameLine += nameLine[t - 1];
+                    }
+                    String strDataLine = new String(dataLine);
+                    System.out.println(strDataLine);
+                    System.out.println(strNameLine);
+                    System.out.println(bufSum);
+                    ArrayList<String> reAnswerList = new ArrayList<String>();
+                    reAnswerList.add(strDataLine);
+                    reAnswerList.add(strNameLine);
+                    reAnswerList.add(bufSum);
+                    answerList.add(reAnswerList);
+                }
+            }
+        }
+        for(int t = 0; t<answerList.size(); t++)
+        {
+            System.out.println(answerList.get(t).get(0) + " " + answerList.get(t).get(1) + " " + answerList.get(t).get(2));
+        }
+        double resSum = 0;
+        ArrayList<String> lastAnswerList = new ArrayList<String>();
+        System.out.println("\n \n \n");
+        for(Integer t = 0; t < answerList.size(); t++)
+        {
+            resSum += Double.parseDouble(answerList.get(t).get(2));
+            for(Integer l = t+1; l < answerList.size(); l++)
+            {
+                if(answerList.get(t).get(1).equals(answerList.get(l).get(1))
+                        && answerList.get(t).get(0).toCharArray()[3] == answerList.get(l).get(0).toCharArray()[3]
+                        && answerList.get(t).get(0).toCharArray()[4] == answerList.get(l).get(0).toCharArray()[4]
+                        && answerList.get(t).get(0).toCharArray()[8] == answerList.get(l).get(0).toCharArray()[8]
+                        && answerList.get(t).get(0).toCharArray()[9] == answerList.get(l).get(0).toCharArray()[9])
+                {
+                    resSum += Double.parseDouble(answerList.get(l).get(2));
+                    answerList.remove(answerList.get(l));
+                    l--;
+                }
+            }
+            lastAnswerList.add(answerList.get(t).get(0) + " " + answerList.get(t).get(1) + " " + String.format("%.2f",resSum));
+            if(answerList.get(t).get(1).contains(nameCompany)){
+                System.out.println(result);
+                result += lastAnswerList.get(t);
+                result += "\n";
+            }
+            resSum = 0;
+            answerList.remove(answerList.get(t));
+        }
+        System.out.println(result);
+        return result;
+    }
+}
